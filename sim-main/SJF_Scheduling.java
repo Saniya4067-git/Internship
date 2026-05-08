a9a3ed0d1e60650edb64f9b1407af4a13b06fd97
Saniya4067-git <24013067@pvgcoet.ac.in>
2026-05-08 17:11:59 +0530

    Add files via upload

--- sim-main/SJF_Scheduling.java
+++ sim-main/SJF_Scheduling.java
@@ -1,17 +1,16 @@
 package org.cloudbus.cloudsim.examples;
 
-import java.util.*;
-
 import org.cloudbus.cloudsim.*;
 import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.provisioners.*;
+
+import java.util.*;
 
 public class SJF_Scheduling {
 
     public static void main(String[] args) {
 
         try {
-
             int num_user = 1;
             Calendar calendar = Calendar.getInstance();
             boolean trace_flag = false;
@@ -21,28 +20,23 @@ public class SJF_Scheduling {
             Datacenter datacenter0 = createDatacenter("Datacenter_0");
 
             DatacenterBroker broker = new DatacenterBroker("Broker");
-
             int brokerId = broker.getId();
 
-            List<Vm> vmlist = new ArrayList<Vm>();
+            // ================= VM LIST =================
+            List<Vm> vmlist = new ArrayList<>();
 
             Vm vm = new Vm(
-                    0,
-                    brokerId,
-                    1000,
-                    1,
-                    512,
-                    1000,
-                    10000,
+                    0, brokerId, 1000, 1,
+                    512, 1000, 10000,
                     "Xen",
                     new CloudletSchedulerTimeShared()
             );
 
             vmlist.add(vm);
+            broker.submitGuestList(vmlist);
 
-            broker.submitVmList(vmlist);
-
-            List<Cloudlet> cloudletList = new ArrayList<Cloudlet>();
+            // ================= CLOUDLET LIST =================
+            List<Cloudlet> cloudletList = new ArrayList<>();
 
             long[] lengths = {4000, 2000, 3000, 1000};
 
@@ -64,46 +58,37 @@ public class SJF_Scheduling {
                 cloudletList.add(cloudlet);
             }
 
+            // ================= SJF SCHEDULING =================
             Collections.sort(cloudletList, new Comparator<Cloudlet>() {
-
+                @Override
                 public int compare(Cloudlet c1, Cloudlet c2) {
-
-                    if (c1.getCloudletLength() < c2.getCloudletLength())
-                        return -1;
-                    else if (c1.getCloudletLength() > c2.getCloudletLength())
-                        return 1;
-                    else
-                        return 0;
+                    return Long.compare(c1.getCloudletLength(), c2.getCloudletLength());
                 }
             });
 
             broker.submitCloudletList(cloudletList);
 
+            // ================= SIMULATION =================
             CloudSim.startSimulation();
 
-            List<Cloudlet> resultList =
-                    broker.getCloudletReceivedList();
+            List<Cloudlet> resultList = broker.getCloudletReceivedList();
 
             CloudSim.stopSimulation();
 
             printCloudletList(resultList);
 
         } catch (Exception e) {
-
             e.printStackTrace();
         }
     }
 
+    // ================= DATACENTER =================
     private static Datacenter createDatacenter(String name) {
 
-        List<Host> hostList = new ArrayList<Host>();
+        List<Host> hostList = new ArrayList<>();
 
-        List<Pe> peList = new ArrayList<Pe>();
-
-        peList.add(new Pe(
-                0,
-                new PeProvisionerSimple(1000)
-        ));
+        List<Pe> peList = new ArrayList<>();
+        peList.add(new Pe(0, new PeProvisionerSimple(1000)));
 
         Host host = new Host(
                 0,
@@ -118,21 +103,14 @@ public class SJF_Scheduling {
 
         DatacenterCharacteristics characteristics =
                 new DatacenterCharacteristics(
-                        "x86",
-                        "Linux",
-                        "Xen",
+                        "x86", "Linux", "Xen",
                         hostList,
-                        10.0,
-                        3.0,
-                        0.05,
-                        0.001,
-                        0.0
+                        10.0, 3.0, 0.05, 0.001, 0.0
                 );
 
         Datacenter datacenter = null;
 
         try {
-
             datacenter = new Datacenter(
                     name,
                     characteristics,
@@ -140,15 +118,14 @@ public class SJF_Scheduling {
                     new LinkedList<Storage>(),
                     0
             );
-
         } catch (Exception e) {
-
             e.printStackTrace();
         }
 
         return datacenter;
     }
 
+    // ================= OUTPUT =================
     private static void printCloudletList(List<Cloudlet> list) {
 
         System.out.println("\nCloudlet ID\tSTATUS\tVM ID\tTime");
@@ -156,12 +133,16 @@ public class SJF_Scheduling {
         for (Cloudlet cloudlet : list) {
 
             System.out.println(
-                    cloudlet.getCloudletId()
-                    + "\tSUCCESS\t"
-                    + cloudlet.getVmId()
-                    + "\t"
-                    + cloudlet.getActualCPUTime()
+                    cloudlet.getCloudletId() + "\tSUCCESS\t" +
+                    cloudlet.getVmId() + "\t" +
+                    cloudlet.getActualCPUTime()
             );
         }
     }
 }
+
+//javaproject:CloudSimProject
+//package:org.cloudbus.cloudsim.examples
+//paste org to src
+//cllassname:SJF_Scheduling
+//runas:java application
